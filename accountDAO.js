bbf8a1242b5fdf5a6129b4c73d254a44d62b9f38
NathanMakesStuff <nathantigner518@gmail.com>
2024-02-20 13:12:27 -0800

    changed name of files, added login variable

---
+++ accountDAO.js
@@ -0,0 +1,79 @@
+const { DynamoDB, DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, QueryCommand, GetCommand, PutCommand } = require("@aws-sdk/lib-dynamodb");
+
+
+const client = new DynamoDBClient({ region: "us-east-1" })
+
+const documentClient = DynamoDBDocumentClient.from(client);
+
+
+
+// check if name exists by checking it against names in db
+async function exists(name) {
+    // if name doesn't exist in db return false
+    const getCommand = new GetCommand({
+        TableName: 'Accounts',
+        Key: { 'username': `${name}` }
+    })
+
+    const promise_created = await documentClient.send(getCommand)
+    if (promise_created.Item == null) {
+        console.log(promise_created.Item, " Item = null")
+        return false;
+    } else {
+        console.log(promise_created.Item, "Item found")
+        return true
+    };
+
+}
+
+
+// register an account by taking in username and password from input, then checking if username exists, if it
+// doesn't then a new account is created
+async function registerAcc(name, password) {
+    const promise = await exists(name);
+    if (!promise) {
+        // add account to db
+        console.log("before putcommand")
+        const putCommand = new PutCommand({
+            TableName: 'Accounts',
+            Item: {
+                username: name,
+                password: password
+            },
+
+        });
+        console.log("before response")
+        const response = await documentClient.send(putCommand);
+        console.log(response)
+        return true; // Created
+    }else{
+        return false; // Error
+    }
+}
+
+// registerAcc("elen", "dog")
+
+
+async function login(name, password) {
+    // open up db, check if name and password match a name and password in account
+    const getCommand = new GetCommand({
+        TableName: 'Accounts',
+        Key: { 'username': `${name}` }
+    })
+
+    const promise_created = await documentClient.send(getCommand);
+    if (promise_created.Item.username == name && promise_created.Item.password == password) {
+        console.log("Logged in")
+        return true;
+    } else {
+        console.log("Wrong info")
+        return false;
+    }
+}
+// login("elen", "dog")
+
+module.exports = {
+    registerAcc,
+    login
+}
--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require('http')
-const {registerAcc, login, exists} = require("./account")
+const { registerAcc, login, exists } = require("./accountDAO")
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+    let currentUser = null;
     if (req.method === "POST" && req.url === "/register") {
         // POST ENDPOINT
         let body = "";
@@ -15,7 +16,7 @@ const server = http.createServer((req, res) => {
             const data = JSON.parse(body);
             const created = await registerAcc(data.username, data.password);
             if (!created) {
-                res.writeHead(201, { "Content-Type": "application/json" });
+                res.writeHead(400, { "Content-Type": "application/json" });
                 res.end(JSON.stringify({ message: "Username already exists" }));
             } else {
                 registerAcc(data.username, data.password);
@@ -23,6 +24,28 @@ const server = http.createServer((req, res) => {
                 console.log(data);
                 res.writeHead(201, { "Content-Type": "application/json" });
                 res.end(JSON.stringify({ message: "Account Created Successfully!" }));
+            }
+        })
+    } else if (req.method === "POST" && req.url === "/login") {
+        // POST ENDPOINT
+        let body = "";
+        req.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        req.on("end", async () => {
+            const data = JSON.parse(body);
+            const loggedIn = await login(data.username, data.password);
+            if (!loggedIn) {
+                res.writeHead(400, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ message: "Invalid Credentials" }));
+            } else {
+                registerAcc(data.username, data.password);
+                // this is where you would save the data
+                console.log(data);
+                currentUser = data.username;
+                res.writeHead(202, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ message: `Account Logged In Successfully! Current User is ${currentUser}` }));
             }
         })
     }
